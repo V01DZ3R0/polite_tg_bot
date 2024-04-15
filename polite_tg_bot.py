40deccb8c963b42c1425d88a23ad038235a9c1dc
V01DZ3R0 <91835641+V01DZ3R0@users.noreply.github.com>
2024-04-15 23:16:07 +0300

    upd

--- polite_tg_bot.py
+++ polite_tg_bot.py
@@ -6,17 +6,21 @@ from pyrogram.types import ChatPermissions
 
 api_id=input("Введите api_id: ")
 api_hash=input("Введите api_hash: ")
+
 app = Client("my_bot", api_id, api_hash)
 
 # Список нецензурных слов для примера
-bad_words = ["хуй","блять","пизда","сука","ебать","нецензурное_слово"]
+bad_words = ["хуй","бля","пизд","сука","ебать","ебан","ёб","хуи","хуя","хуе","ебл","залупа","нецензурное_слово"]
 
 # Хранение информации о пользователях
 user_data = {}
 
 @app.on_message(filters.text & ~filters.private)
+
+
 async def check_message(client, message):
-    user_id = message.from_user.id
+    if message.from_user is not None:
+        user_id = message.from_user.id
     text = message.text.lower()
     time = message.date
     bad_words_count = sum(word in text for word in bad_words)
@@ -31,10 +35,10 @@ async def check_message(client, message):
         until_date = now + timedelta(days=0.5)
 
         # Создаем объект ChatPermissions с нужными правами
-        permissions = ChatPermissions(can_send_messages=False)
-
+                
         # Предполагаем, что client и message уже определены
-        await client.restrict_chat_member(message.chat.id, user_id, permissions, until_date)
+        await client.restrict_chat_member(message.chat.id, user_id, ChatPermissions(can_send_messages=False), until_date)
         user_data[user_id]["count"] = 0
         await message.reply("Вы были ограничены в доступе к чату на 12 часов за чрезмерное использование нецензурной лексики.")
+        
 app.run()
